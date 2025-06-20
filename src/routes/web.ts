c49ae6dc8ef347fd7ee1d3236d716ce7389c9f83
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2025-06-20 10:10:38 +0900

    feat: remove unused authentication routes from web router

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,11 +1,9 @@
 import express from 'express'
 
-import authRoutes from './authRouter'
 import testRoutes from './test'
 
 const web = express.Router()
 
 web.use('/test', testRoutes)
-web.use('/auth', authRoutes)
 
 export default web
